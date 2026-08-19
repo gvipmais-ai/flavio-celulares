d788bae1f63813ff5cd0a54e26274a3809e52420
Jerry Montalvao <jerrymontalvao@Mac-mini-de-Jerry.local>
2026-08-19 17:19:06 -0300

    chore: update next.config.ts to fix images.domains deprecation warning

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['flavio-celulares.vercel.app'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flavio-celulares.vercel.app',
+      },
+    ],
   },
 };
 
